9cb9a8502211a59f2bb5a9f87d659c1688164050
nobody <nobody@nowhere.mars>
2018-09-30 20:23:06 +0800

    Edit version number.

--- V2RayGCon/Properties/Resources.Designer.cs
+++ V2RayGCon/Properties/Resources.Designer.cs
@@ -126,7 +126,7 @@ namespace V2RayGCon.Properties {
         }
         
         /// <summary>
-        ///   查找类似 1.0.32 的本地化字符串。
+        ///   查找类似 1.0.31.1 的本地化字符串。
         /// </summary>
         internal static string Version {
             get {
