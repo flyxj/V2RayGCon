cd4b80c33da7228f87c5b19e5d0cca2f46c1893d
nobody <nobody@nowhere.mars>
2018-11-20 11:21:52 +0800

    Bump version.

--- V2RayGCon/Properties/Resources.Designer.cs
+++ V2RayGCon/Properties/Resources.Designer.cs
@@ -284,7 +284,7 @@ namespace V2RayGCon.Properties {
         }
         
         /// <summary>
-        ///   查找类似 1.1.2.7 的本地化字符串。
+        ///   查找类似 1.1.3 的本地化字符串。
         /// </summary>
         internal static string Version {
             get {
